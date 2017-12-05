ecaa0051a7660fd818602f5682917028ee7a8ce8
Samuel Johnson <samjohnson@Samuels-MacBook-Pro.local>
2017-12-05 15:14:02 +0000

    Used a form to post cat name

--- app.rb
+++ app.rb
@@ -14,8 +14,13 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  @name = params[:name]
+  erb(:cat_form)
 end
